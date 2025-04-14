d7cec79fd93f98eded8564a7f5364d6d7184044e
Miguel <m9.0serrano@gmail.com>
2025-04-14 15:34:42 -0500

    Server Redeploy

--- server/server.js
+++ server/server.js
@@ -1,39 +1,41 @@
 // server.js
-const express = require('express');
-const cors = require('cors');
-const connectDB = require('./connections/MongoConn.js');
-const registrationRoutes = require('./routes/registration.js');
-const bodyParser = require('body-parser');
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./connections/MongoConn.js");
+const registrationRoutes = require("./routes/registration.js");
+const bodyParser = require("body-parser");
 
 // const cookieParser = require('cookie-parser');
 // const csurf = require('csurf');
-require('dotenv').config();
+require("dotenv").config();
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-connectDB()
+connectDB();
 
+app.options('*', cors());  // Handle preflight for all routes
 // Middleware
-app.use(cors({
-  origin: [
-    'https://serranoartcamp.org',
-    'https://www.serranoartcamp.org',
-    'http://localhost:5173'
-  ],
-  methods: ['GET', 'POST', 'PUT']
-}));
+app.use(
+  cors({
+    origin: [
+      "https://www.serranoartcamp.org/api",
+      "https://www.serranoartcamp.org",
+      "https://serrano-art-camp-*.vercel.app",
+    ],
+    methods: ["GET", "POST", "PUT", "OPTIONS"],
+  })
+);
 app.use(bodyParser.json());
 // app.use(cookieParser());
 // app.use(csurf({ cookie: true }));
 
-app.use('/api/registrations', registrationRoutes); 
+app.use("/api/registrations", registrationRoutes);
 
-
-app.get('/api/health', (req, res) => {
-  res.status(200).json({ status: 'OK', message: 'Server is running' });
+app.get("/api/health", (req, res) => {
+  res.status(200).json({ status: "OK", message: "Server is running" });
 });
 
 if (process.env.LOCAL_DEVELOPMENT) {
@@ -45,5 +47,4 @@ if (process.env.LOCAL_DEVELOPMENT) {
   });
 }
 
-
-module.exports = app
+module.exports = app;
